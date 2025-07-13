4f2956f44023a241f2b1a9588dcc5458b80af21c
eftekar1473 <eftekarhossen3223@gmail.com>
2025-07-14 01:21:07 +0600

    Initial commit

---
+++ src/SpaceInvaders.java
@@ -0,0 +1,250 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class SpaceInvaders extends JPanel implements ActionListener, KeyListener {
+    int tileSize = 32;
+    int rows = 16;
+    int columns = 16;
+
+    int boardWidth = tileSize * columns;
+    int boardHeight = tileSize * rows;
+
+    Image shipImg;
+    Image alienImg;
+    Image alienCyanImg;
+    Image alienMagentaImg;
+    Image alienYellowImg;
+    ArrayList<Image> alienImgArray;
+
+    class Block {
+        int x, y, width, height;
+        Image img;
+        boolean alive = true;
+        boolean used = false;
+
+        Block(int x, int y, int width, int height, Image img) {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.img = img;
+        }
+    }
+
+    int shipWidth = tileSize * 2;
+    int shipHeight = tileSize;
+    int shipX = tileSize * columns / 2 - tileSize;
+    int shipY = tileSize * rows - tileSize * 2;
+    int shipVelocityX = tileSize;
+    Block ship;
+
+    ArrayList<Block> alienArray;
+    int alienWidth = tileSize * 2;
+    int alienHeight = tileSize;
+    int alienX = tileSize;
+    int alienY = tileSize;
+
+    int alienRows = 2;
+    int alienColumns = 3;
+    int alienCount = 0;
+    int alienVelocityX = 1;
+
+    ArrayList<Block> bulletArray;
+    int bulletWidth = tileSize / 8;
+    int bulletHeight = tileSize / 2;
+    int bulletVelocityY = -10;
+
+    Timer gameLoop;
+    boolean gameOver = false;
+    int score = 0;
+
+    int maxBullets = 0;
+    int bulletsUsed = 0;
+    boolean bulletsEnded = false;
+
+    SpaceInvaders() {
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+        setBackground(Color.black);
+        setFocusable(true);
+        addKeyListener(this);
+
+        shipImg = new ImageIcon(getClass().getResource("./ship.png")).getImage();
+        alienImg = new ImageIcon(getClass().getResource("./alien.png")).getImage();
+        alienCyanImg = new ImageIcon(getClass().getResource("./alien-cyan.png")).getImage();
+        alienMagentaImg = new ImageIcon(getClass().getResource("./alien-magenta.png")).getImage();
+        alienYellowImg = new ImageIcon(getClass().getResource("./alien-yellow.png")).getImage();
+
+        alienImgArray = new ArrayList<>();
+        alienImgArray.add(alienImg);
+        alienImgArray.add(alienCyanImg);
+        alienImgArray.add(alienMagentaImg);
+        alienImgArray.add(alienYellowImg);
+
+        ship = new Block(shipX, shipY, shipWidth, shipHeight, shipImg);
+        alienArray = new ArrayList<>();
+        bulletArray = new ArrayList<>();
+
+        gameLoop = new Timer(1000 / 60, this);
+        createAliens();
+        gameLoop.start();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(ship.img, ship.x, ship.y, ship.width, ship.height, null);
+
+        for (Block alien : alienArray) {
+            if (alien.alive) {
+                g.drawImage(alien.img, alien.x, alien.y, alien.width, alien.height, null);
+            }
+        }
+
+        g.setColor(Color.white);
+        for (Block bullet : bulletArray) {
+            if (!bullet.used) {
+                g.drawRect(bullet.x, bullet.y, bullet.width, bullet.height);
+            }
+        }
+
+        g.setColor(Color.white);
+        g.setFont(new Font("Arial", Font.PLAIN, 32));
+        if (gameOver) {
+            g.drawString("Game Over: " + score, 10, 35);
+        } else {
+            g.drawString(String.valueOf(score), 10, 35);
+        }
+
+        g.setFont(new Font("Arial", Font.PLAIN, 20));
+        g.drawString("Bullets: " + (maxBullets - bulletsUsed), 10, boardHeight - 10);
+
+        if (bulletsEnded && alienCount > 0 && bulletArray.isEmpty()) {
+            g.setFont(new Font("Arial", Font.BOLD, 32));
+            g.setColor(Color.red);
+            g.drawString("Bullets Ended", boardWidth / 2 - 100, boardHeight / 2);
+            g.setFont(new Font("Arial", Font.PLAIN, 24));
+            g.drawString("Press any key to restart", boardWidth / 2 - 130, boardHeight / 2 + 40);
+        }
+    }
+
+    public void move() {
+        for (Block alien : alienArray) {
+            if (alien.alive) {
+                alien.x += alienVelocityX;
+                if (alien.x + alien.width >= boardWidth || alien.x <= 0) {
+                    alienVelocityX *= -1;
+                    alien.x += alienVelocityX * 2;
+                    for (Block a : alienArray) {
+                        a.y += alienHeight;
+                    }
+                }
+                if (alien.y >= ship.y) {
+                    gameOver = true;
+                }
+            }
+        }
+
+        for (Block bullet : bulletArray) {
+            bullet.y += bulletVelocityY;
+            for (Block alien : alienArray) {
+                if (!bullet.used && alien.alive && detectCollision(bullet, alien)) {
+                    bullet.used = true;
+                    alien.alive = false;
+                    alienCount--;
+                    score += 100;
+                }
+            }
+        }
+
+        while (!bulletArray.isEmpty() && (bulletArray.get(0).used || bulletArray.get(0).y < 0)) {
+            bulletArray.remove(0);
+        }
+
+        if (alienCount == 0) {
+            score += alienColumns * alienRows * 100;
+            alienColumns = Math.min(alienColumns + 1, columns / 2 - 2);
+            alienRows = Math.min(alienRows + 1, rows - 6);
+            alienArray.clear();
+            bulletArray.clear();
+            createAliens();
+        }
+    }
+
+    public void createAliens() {
+        Random random = new Random();
+        for (int c = 0; c < alienColumns; c++) {
+            for (int r = 0; r < alienRows; r++) {
+                int randomImgIndex = random.nextInt(alienImgArray.size());
+                Block alien = new Block(
+                    alienX + c * alienWidth,
+                    alienY + r * alienHeight,
+                    alienWidth,
+                    alienHeight,
+                    alienImgArray.get(randomImgIndex)
+                );
+                alienArray.add(alien);
+            }
+        }
+        alienCount = alienArray.size();
+        maxBullets = (int) Math.ceil(alienCount * 2);
+        bulletsUsed = 0;
+        bulletsEnded = false;
+    }
+
+    public boolean detectCollision(Block a, Block b) {
+        return a.x < b.x + b.width &&
+               a.x + a.width > b.x &&
+               a.y < b.y + b.height &&
+               a.y + a.height > b.y;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        move();
+        repaint();
+        if (gameOver || (bulletsEnded && alienCount > 0 && bulletArray.isEmpty())) {
+            gameLoop.stop();
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {}
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (gameOver || (bulletsEnded && alienCount > 0 && bulletArray.isEmpty())) {
+            ship.x = shipX;
+            bulletArray.clear();
+            alienArray.clear();
+            gameOver = false;
+            score = 0;
+            alienColumns = 3;
+            alienRows = 2;
+            alienVelocityX = 1;
+            createAliens();
+            gameLoop.start();
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT && ship.x - shipVelocityX >= 0) {
+            ship.x -= shipVelocityX;
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT && ship.x + shipVelocityX + ship.width <= boardWidth) {
+            ship.x += shipVelocityX;
+        } else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            if (bulletsUsed < maxBullets) {
+                Block bullet = new Block(ship.x + shipWidth * 15 / 32, ship.y, bulletWidth, bulletHeight, null);
+                bulletArray.add(bullet);
+                bulletsUsed++;
+                if (bulletsUsed >= maxBullets) {
+                    bulletsEnded = true;
+                }
+            }
+        }
+    }
+}
